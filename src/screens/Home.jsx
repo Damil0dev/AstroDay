9bad57cc68937a26fd58ba3859556653e8cc6475
Damian Ysla <ysladamian@outlook.com>
2023-10-11 18:17:07 -0300

    made home screen

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,9 +1,19 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, TouchableHighlight, TouchableOpacity } from "react-native";
+
+import { ImgCard } from "../components/ImgCard";
 
 export function Home() {
     return(
-        <View>
-            <Text>Home</Text>
+        <View className="h-full bg-blue-800">
+            <TouchableOpacity className="p-5">
+                <Text className="p-2 text-xl text-white">Today's Image</Text>
+                <ImgCard title={"Title"} />
+            </TouchableOpacity>
+
+            <TouchableOpacity className="p-5">
+                <Text className="p-2 text-xl text-white">Most Liked Image This Week</Text>
+                <ImgCard title={"Title"} />
+            </TouchableOpacity>
         </View>
     )
 }
