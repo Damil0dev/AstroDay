a280423fc2c894ca51a5c5572fd2369e5a072d9c
Damian Ysla <ysladamian@outlook.com>
2023-10-11 15:40:24 -0300

    made header

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { Header } from "./src/components/Header";
 import { Home } from "./src/screens/Home";
 
 const Stack = createNativeStackNavigator();
@@ -9,6 +10,8 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
+      <Header />
+
       <Stack.Navigator>
         <Stack.Screen name='Home' component={Home} options={{
           headerShown: false
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { Text, View, Image } from "react-native";
+
+import menuIcon from "../../assets/menu.png"
+import userDefaultIcon from "../../assets/profile-user.png"
+
+export function Header() {
+    return (
+        <View className="flex flex-row justify-between items-center p-4 bg-blue-600">
+            <Image source={menuIcon} className="translate-y-1"></Image>
+            <Text className="text-2xl italic font-medium border-b-4 border-fuchsia-600 text-white">AstroDay</Text>
+            <Image source={userDefaultIcon}></Image>
+        </View>
+    )
+}
