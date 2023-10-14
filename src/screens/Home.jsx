89abfa871f0287c391ae0b8dcdbbbcc8e64736c7
Damian Ysla <ysladamian@outlook.com>
2023-10-14 16:34:23 -0300

    add random img of the day

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,16 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { Text, View, Image, TouchableHighlight, TouchableOpacity, ScrollView } from "react-native";
 
 import { ImgCard } from "../components/ImgCard";
-
 import { getAstronomyPictureOfDay } from '../api/api.js';
 
 export function Home() {
     const [data, setData] = useState([]);
+    const [randomData, setRandomData] = useState([]);
+
+    const getRandomDate = () => {
+        const start = new Date("1995-01-01");
+        const end = new Date();
+        const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+        return date.toISOString().split('T')[0];
+    }
 
     useEffect(() => {
+        const todayDate = new Date().toISOString().split('T')[0];
+        const generatedRandomDate = getRandomDate();
+
         const fetchData = async () => {
             try {
-                const result = await getAstronomyPictureOfDay('2023-01-01'); // O cualquier otra fecha
+                const result = await getAstronomyPictureOfDay(todayDate);
                 setData(result);
                 console.log(result);
             } catch (error) {
@@ -19,14 +29,29 @@ export function Home() {
             }
         };
 
+        const fetchData2 = async () => {
+            try {
+                const result = await getAstronomyPictureOfDay(generatedRandomDate);
+                setRandomData(result);
+                console.log(result);
+            } catch (error) {
+                console.error(error.message);
+            }
+        };
+
         fetchData();
+        fetchData2()
     }, []);
 
-    return(
+    return (
         <ScrollView className="h-full bg-blue-800">
             <TouchableOpacity className="p-5">
                 <Text className="p-2 text-xl text-white">Today's Image</Text>
                 <ImgCard title={data.title} img={data.url} date={data.date} />
+            </TouchableOpacity>
+            <TouchableOpacity className="p-5">
+                <Text className="p-2 text-xl text-white">Random Image</Text>
+                <ImgCard title={randomData.title} img={randomData.url} date={randomData.date} />
             </TouchableOpacity>
             <TouchableOpacity className="p-5">
                 <Text className="p-2 text-xl text-white">Most Liked Image This Week</Text>
